ceda3d6b6eddb53115c0a2844b20daee83746f93
L.Dongming <l.dongming@apecloud.com>
2023-06-11 13:28:20 +0800

    chore: ack use g6 to fetch az

--- alicloud/ack/main.tf
+++ alicloud/ack/main.tf
@@ -16,7 +16,7 @@ data "alicloud_cs_cluster_credential" "auth" {
 
 // Available zones for current region
 data "alicloud_zones" "this" {
-  available_instance_type     = var.worker_instance_types.0
+  available_instance_type     = var.worker_instance_types.1
   available_disk_category     = "cloud_efficiency"
   available_resource_creation = "VSwitch"
 }
