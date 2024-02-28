3ce45026700b56308bef95017ed5f4811ffcf26b
huangzhangshu <huangzhangshu@apecloud.com>
2024-02-28 19:24:19 +0800

    chore: support azure terraform init

---
+++ azure/aks/aks.tf
@@ -0,0 +1,57 @@
+# AKS cluster
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+resource "random_pet" "prefix" {}
+
+resource "azurerm_resource_group" "default" {
+  name     = "${local.cluster_name}-group"
+  location = local.region
+
+  tags = {
+    environment = "cicd-test"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = local.cluster_name
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "${random_pet.prefix.id}-k8s"
+  kubernetes_version  = local.cluster_version
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 1
+    vm_size         = "Standard_B2s"
+    os_disk_size_gb = 50
+    enable_auto_scaling = false
+  }
+
+  service_principal {
+    client_id     = local.appId
+    client_secret = local.password
+  }
+
+  role_based_access_control_enabled = true
+
+  tags = {
+    environment = "cicd-test"
+  }
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "test-pool" {
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.default.id
+  name                  = local.node_pool_name
+  vm_size               = local.machine_type
+  node_count            = local.node_count
+  os_disk_size_gb       = local.disk_size_gb
+#  priority              = "Spot" # Spot|Regular
+#  eviction_policy       = "Delete"
+#  spot_max_price        = 0.5 # note: this is the "maximum" price
+#  node_labels = {
+#    "kubernetes.azure.com/scalesetpriority" = "spot"
+#  }
+}
---
+++ azure/aks/main.tf
@@ -0,0 +1,19 @@
+# # Kubernetes provider
+
+locals {
+  appId                     = var.appId
+  password                  = var.password
+  name                      = "cicd-aks-${random_string.suffix.result}"
+  cluster_name              = lower(coalesce(var.cluster_name, local.name))
+  cluster_version           = var.cluster_version
+  region                    = var.region
+  node_pool_name            = var.node_pool_name
+  machine_type              = var.machine_type
+  disk_size_gb              = var.disk_size_gb
+  node_count                = var.node_count
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
